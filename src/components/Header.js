be0a8902591df6c0f7bede9cfc4ba93cf1bcf888
b4a <74990094+Bianca-ME@users.noreply.github.com>
2023-02-22 20:10:05 +0200

    logo redirect to home page
    meanwhile:
    npm install --save-dev file-loader
    
    code must be cleaned from all code that is commented out (code that did not work)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link} from 'react-router-dom';
 
-export default function Header () {
+// const history = useHistory();
+
+// import logo from './logo.png';
+import logo from '../assets/logo.png';
+
+
+// console.log(logo);
+
+export default function Header() {
     return (
         <div className='header'>
+            {/* <a href="https://codingbeautydev.com" target="_blank" rel="noreferrer">
+                <img src={logo} alt="Coding Beauty logo"></img>
+            </a> */}
+            <Link to="/" rel="noreferrer">
+                <img src={logo} alt="Nature"></img>
+            </Link>
+
+
             <NavLink to='/about'>Despre Ultrafilter</NavLink>
             <NavLink to='/utile'>linkuri utile</NavLink>
             <NavLink to='/contact'>Contact</NavLink>
+            {/* <Navlink to='/'>
+                <img src={logo} alt='logo'/>logotext
+            </Navlink> */}
         </div>
     )
 }
