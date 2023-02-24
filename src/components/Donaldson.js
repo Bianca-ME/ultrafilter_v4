137adc6e2184d81027c8c4f1fbaab151e9ff6160
b4a <74990094+Bianca-ME@users.noreply.github.com>
2023-02-24 21:26:25 +0200

    table test
    just playin'

--- src/components/Donaldson.js
+++ src/components/Donaldson.js
@@ -13,10 +13,36 @@ export default function Donaldson () {
             <main className='card text-donaldson'>
                 <p>design page --- WORK IN PROGRESS</p>
                 <p>content of DONALDSON</p>
-                <NavLink to='/Donaldson/compresoare'>compresoare</NavLink>
+                
                 <br/>
                 <NavLink to='/Donaldson/filtrare-si-uscare-aer-comprimat-purificare-aer-comprimat'>filtrare si uscare aer comprimat purificare aer comprimat</NavLink>
-
+                <br/>
+                <br/>
+                <table className='tabel-donaldson-compresoare'>
+                    <tr>
+                        <td colspan='2'><NavLink to='/Donaldson/compresoare'>compresoare</NavLink></td>
+                    </tr>
+                    <tr>
+                        <td>presiuni 4 - 13 bari</td>
+                        <td>presiuni 20 - 400 bari</td>
+                    </tr>
+                    <tr>
+                        <td>compresoare cu piston</td>
+                        <td>compresoare cu piston</td>
+                    </tr>
+                    <tr>
+                        <td>compresoare cu suruburi elicoidale</td>
+                        <td>compresoare cabinete stomatologice</td>
+                    </tr>
+                    <tr>
+                        <td>compresoare cu suruburi elicoidale fara ulei</td>
+                        <td>compresoare de spart asfaltul - motocompresoare</td>
+                    </tr>
+                    <tr>
+                        <td>scroll compresor - fara ulei</td>
+                        <td></td>
+                    </tr>
+                </table>
                 
             </main>
         // </Router>
