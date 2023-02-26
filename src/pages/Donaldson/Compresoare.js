5c706c613065d7976d7e05472e750efd390da5ef
b4a <74990094+Bianca-ME@users.noreply.github.com>
2023-02-26 18:46:51 +0200

    embedded pdf from google drive. test
    temporarily solution. it is disturbing that it has to verify the user if it's a human and also I don't like the format how it's embedded. but at least for the moment the pdf can be viewed.

--- src/pages/Donaldson/Compresoare.js
+++ src/pages/Donaldson/Compresoare.js
@@ -1,11 +1,14 @@
 import React from 'react';
+// import './../../../public/testPDF.pdf';
 
 export default function Compresoare() {
   return (
     <div>
-      <object data="../../../testPDF.pdf" type="application/pdf" width="100%" height="600px">
-        <p>Alternative text - include a link <a href="../../../testPDF.pdf">to the PDF!</a></p>
-      </object>
+      {/* <object data="https://drive.google.com/file/d/1rZMWHRzH_rGtFoxc867ZRAT2YtEnPOA1" width="100%" height="600px">
+        <p>Alternative text - include a link <a href="https://drive.google.com/file/d/1rZMWHRzH_rGtFoxc867ZRAT2YtEnPOA1">to the PDF!</a></p>
+      </object> */}
+      <iframe src="https://drive.google.com/file/d/1rZMWHRzH_rGtFoxc867ZRAT2YtEnPOA1/preview" width="640" height="480" allow="autoplay"></iframe>
+      {/* <a href="https://github.com/Bianca-ME/-ultrafilter-react-app-t20/blob/t20.v1.1.1-adding-pages/public/testPDF.pdf" target="_blank">Download PDF</a> */}
     </div>
   );
 }
